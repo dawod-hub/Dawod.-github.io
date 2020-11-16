2998cd75c784af9a557b0f73ce8ef00266ae9003
dawod-hub <73028587+dawod-hub@users.noreply.github.com>
2020-11-16 19:44:04 +0530

    Add files via upload

---
+++ travel.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>travelsite</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<style>
+
+</style>
+</head>
+
+<body>
+
+
+<nav class ="nvarbar navbar-expand-sm nvarbar-dark bg-dark">
+<div class ="container">
+
+
+<a href ="index.html" class ="navbar-brand">Travel Site</a>
+<button class ="navbar-toggler" data-toggle ="collapse" data-target="#travel-navbar">
+<span ><i class ="navbar-toggler-icon"></i></span>
+</button>
+
+<div class ="collapse navbar-collapse" id ="travel-navbar">
+
+<ul class ="nav nav-tabs ml-auto">
+<li class ="nav-item px-3 ">
+<a href ="#" class ="nav-link">Home</a>
+</li>
+<li class ="nav-item px-3">
+<a href = "#uy" class ="nav-link">search</a>
+</li>
+<li class ="nav item px-3">
+<a href = "https://www.holidify.com/places/hyderabad/sightseeing-and-things-to-do.html" class ="nav-link">visiting spots</a>
+</li>
+<li class ="nav-item">
+<a href = "#" class ="nav-link">Gollery</a>
+</li>
+<li class ="nav-item px-3">
+<a href ="#" class ="nav-link"> contact</a>
+</li>
+</ul>
+</div>
+</div>
+</nav>
+<h1 class ="display-1 text-center">Welcome to Website</h1>
+<div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active center-block" data-interval="10000">
+      <img src="image/dawod3.jpg" class="d-block w-100 " alt="..." height="500px">
+    </div>
+    <div class="carousel-item" data-interval="2000">
+      <img src="image/dawod4.jpg" class="d-block w-100" height="500px">
+    </div>
+    <div class="carousel-item">
+      <img src="image/dawod5.jpg" class="d-block w-100  center-block" alt="..." height="500px" >
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<nav aria-label="Page navigation example" class ="justify-content-center">
+  <ul class="pagination">
+    <li class="page-item"><a class="page-link" href="image/dawod6.jpg">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="image/dawod7.jpg">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  </ul>
+</nav>
+<h2 class ="display-1 text-center text-info">Information</h1>
+<p>    Readers and editors pick the most interesting and insightful articles from the past year about teaching, learning, and the keys to well-being at school.
+BY AMY L. EVA | JANUARY 10, 2020
+Print Bookmark
+Looking for inspiration to start the new decade off on the right foot? Our most popular education articles of 2019 explore how children develop purpose, how we can best support our students’ mental health and social-emotional development, why we benefit from listening to each other’s stories, and more.
+
+
+And…if you want to put the scientific findings from these articles into practice, check out our new website for educators, Greater Good in Education (GGIE), officially launching on February 20, 2020.
+
+In response to our readers’ call for more practical resources for the classroom, GGIE features free research-based practices, lessons, and strategies for educators to foster their students’ and their own well-being, and for school leaders to develop positive school climates—all in the service of cultivating kinder, happier, and more equitable classrooms and schools.
+</p>
+<div class="row">
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="https://www.gizbot.com/photos/xiaomi-mi-10t-pro-5g-images-5642/">
+        <img src="image/dawod7.jpg"  alt ="" style="width:100%; height:300px;">
+        <div class="caption">
+          <p>Lorem ipsum...</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="https://ndl.iitkgp.ac.in/">
+        <img src="image/dawod6.jpeg"  style="width:100%; height:300px;" >
+        <div class="caption">
+          <p>Lorem ipsum...</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="circle">
+      <a href="https://www.holidify.com/places/hyderabad/sightseeing-and-things-to-do.html">
+        <img src="image/dawod5.jpg" alt="" style="width:100%; height:300px;">
+        <div class="caption">
+          <p>Lorem ipsum...</p>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
+
+
+<script>
+
+$('.carousel').carousel();
+</script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+</body>
+</html>
